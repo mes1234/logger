6d47a7c47675ef95e600b25184f8d97513723e24
mes1234 <witold.kepczynski@gmail.com>
2019-01-19 17:37:50 +0100

    test cases modified

--- AddItem.sh
+++ AddItem.sh
@@ -1,1 +1,1 @@
-curl -X PUT -d project_id=1 -d name=test1 -d description="treejrioje erwrwe dsfs" -d date="2017-11-22T09:58:00" -d owner_id=1 http://127.0.0.1:5000/projects/1/items
+curl -X PUT -d project_id=1 -d name=test1 -d description="treejrioje erwrwe dsfs" -d date="2019-11-22T09:58:00" -d owner_id=1 http://127.0.0.1:5000/projects/1/items
